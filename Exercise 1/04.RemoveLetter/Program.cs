ad2f1355c0132d102588a02a1e32d461dda440f7
Gergana Ivanova <geri7423@abv.bg>
2024-02-22 22:08:38 +0200

    add

---
+++ Exercise 1/04.RemoveLetter/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04.RemoveLetter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+            string word = Console.ReadLine();
+            for (int i = 0; i < word.Length; i++)
+            {
+                char c = word[i];
+                if (letter == c)
+                {
+                    int index = word.IndexOf(c);
+                    word = word.Remove(index, 1);
+                }
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
