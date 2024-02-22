9aed4afd42a9477813a286ed2a0c599c3677b9d8
Gergana Ivanova <geri7423@abv.bg>
2024-02-22 22:07:32 +0200

    add

---
+++ Exercise 1/02.SumOfNumbers 2.0/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _02.SumOfNumbers_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Enter number: ");
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine($"Sum is: {sum}");
+
+        }
+    }
+}
