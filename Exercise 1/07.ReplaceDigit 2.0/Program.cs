6a844e0533c1c1f7f8feff1f9bc4aa6d0c8c7aff
Gergana Ivanova <geri7423@abv.bg>
2024-02-22 22:09:53 +0200

    add

---
+++ Exercise 1/07.ReplaceDigit 2.0/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _07.ReplaceDigit_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number: ");
+            string number = Console.ReadLine();
+            for(int i = 0; i < number.Length; i++) 
+            {
+                int digit = int.Parse(number[i].ToString());
+                Console.Write(digit + " ");
+                for(int j = 0; j < digit; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
