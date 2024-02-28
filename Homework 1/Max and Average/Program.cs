1d7c8a6bd301197ae4345d5f05dcca53e39a4c57
Gergana Ivanova <geri7423@abv.bg>
2024-02-28 23:32:03 +0200

    add

---
+++ Homework 1/Max and Average/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Max_and_Average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int count = 0;
+            int number = int.Parse(Console.ReadLine());
+            double average = 0;
+            int maxNumber = int.MinValue;
+            while (number != 0)
+            {
+                count++;
+                sum += number;
+                if(number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+                number = int.Parse(Console.ReadLine());
+            }
+
+            average = (double)sum / count;
+            Console.WriteLine(average);
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
