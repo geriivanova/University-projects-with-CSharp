d0ff4c448d708453a5fa60e58ed4cd088f2ba95b
Gergana Ivanova <geri7423@abv.bg>
2024-02-28 23:30:49 +0200

    add

---
+++ Homework 1/01.Count of vowel/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01.Count_of_vowel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int count = 0;
+            text = text.ToLower();
+            for(int i = 0; i < text.Length; i++)
+            {
+                switch (text[i])
+                {
+                    case 'а':
+                    case 'ъ':
+                    case 'о':
+                    case 'у':
+                    case 'е':
+                    case 'и':
+                    case 'ю':
+                    case 'я':
+                        count++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
