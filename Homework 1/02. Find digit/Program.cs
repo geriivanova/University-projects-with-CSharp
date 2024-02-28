77a05849ac717a76c4ed4d54ee596bc58833668d
Gergana Ivanova <geri7423@abv.bg>
2024-02-28 23:31:26 +0200

    add

---
+++ Homework 1/02. Find digit/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02._Find_digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int digit = int.Parse(Console.ReadLine());
+            int firstDigit, lastDigit;
+            for(int i = 10; i < 99; i++)
+            {
+                lastDigit = i % 10;
+                firstDigit = i / 10;
+                if(firstDigit == digit || lastDigit == digit)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
