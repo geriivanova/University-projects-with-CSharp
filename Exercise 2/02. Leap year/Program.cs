870cb8513e2a8b2de376ddc82a8ebf24d2b8d97a
Gergana Ivanova <geri7423@abv.bg>
2024-02-29 09:59:48 +0200

    new

---
+++ Exercise 2/02. Leap year/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._Leap_year
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine($"{year} is a leap year!");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year!");
+            }
+        }
+    }
+}
