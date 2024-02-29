29fcda5f407fdafdec0af16aaf354c10609d1180
Gergana Ivanova <geri7423@abv.bg>
2024-02-29 09:59:05 +0200

    new

---
+++ Exercise 2/01. Average of numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Average_of_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 0;
+            int.TryParse(Console.ReadLine(), out a);
+            int.TryParse(Console.ReadLine(), out b);
+            int.TryParse(Console.ReadLine(), out c);
+
+            decimal average = 0;
+            decimal sum = 0;
+
+            sum = a + b + c;
+            average = sum / 3;
+            Console.WriteLine($"{average:F2}");
+        }
+    }
+}
