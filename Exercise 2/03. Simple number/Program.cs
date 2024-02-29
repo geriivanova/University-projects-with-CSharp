059c0dcafe51dc21e0bafcfdb8f36487deada6d1
Gergana Ivanova <geri7423@abv.bg>
2024-02-29 10:01:18 +0200

    new

---
+++ Exercise 2/03. Simple number/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03._Simple_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if(number == 0 || number == 1)
+            {
+                Console.WriteLine($"{number} is not a prime number");
+            }
+            else if(number % 2 == 0 || 
+                    number % 3 == 0 || 
+                    number % 4 == 0 || 
+                    number % 5 == 0 || 
+                    number % 6 == 0 ||
+                    number % 7 == 0 ||
+                    number % 8 == 0 ||
+                    number % 9 == 0)
+                 {
+                    Console.WriteLine($"{number} is not a prime number");
+                 }
+            
+            else
+            {
+                Console.WriteLine($"{number} is a prime number");
+            }
+        }
+    }
+}
