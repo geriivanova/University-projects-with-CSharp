cc4f0e3da5f6ad19eeaf96074a982eec65fd1a98
Gergana Ivanova <geri7423@abv.bg>
2024-03-07 12:12:40 +0200

    add

--- Exercise 3/04. Most common symbol/Program.cs
+++ Exercise 3/04. Most common symbol/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace _04._Most_common_symbol
+﻿/*
+a
+e
+r
+r
+t
+s
+r
+t
+s
+t
+r
+y
+u
+i
+e
+i
+a
+e
+r
+t
+ */
+using System.Collections.Immutable;
+
+namespace _04._Most_common_symbol
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char[] symbols = new char[20];
+
+            for(int i = 0; i < symbols.Length; i++)
+            {
+                symbols[i] = char.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(MostCommonSymbol(symbols));
+        }
+
+        public static char MostCommonSymbol(char[] symbols)
+        {
+            return symbols.GroupBy(x => x)
+                .OrderByDescending(x => x.Count())
+                .Select(x => x.Key)
+                .First();
         }
     }
 }
