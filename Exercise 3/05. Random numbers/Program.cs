4f56338fd5bf6e7122003f74e9ded823d4b7dc85
Gergana Ivanova <geri7423@abv.bg>
2024-03-07 12:15:16 +0200

    new project

---
+++ Exercise 3/05. Random numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05._Random_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
