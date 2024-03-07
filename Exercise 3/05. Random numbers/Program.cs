cb0bd1c427ca67c3abb85dd4d61cddab7b5e0f48
Gergana Ivanova <geri7423@abv.bg>
2024-03-07 12:24:55 +0200

    finish

--- Exercise 3/05. Random numbers/Program.cs
+++ Exercise 3/05. Random numbers/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = new int[20];
+            Random random = new Random();
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(-3000,3000);
+                Console.WriteLine("Random number: ");
+                Console.WriteLine(numbers[i]);
+            }
+            
+            for(int i = 0; i <= numbers.Length; i+=2)
+            {
+                Console.WriteLine(numbers[i]);
+            }
         }
     }
 }
