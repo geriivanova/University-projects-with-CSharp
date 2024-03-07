65d265f51d41982c1273da16358bc3ae883fd33e
Gergana Ivanova <geri7423@abv.bg>
2024-03-07 10:12:35 +0200

    create project

---
+++ Exercise 3/01. Check for integers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Check_for_integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
