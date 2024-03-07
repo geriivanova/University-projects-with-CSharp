372cfbb60b972dfa3c42873344d443e87d3a5ba8
Gergana Ivanova <geri7423@abv.bg>
2024-03-07 10:45:59 +0200

    create project

---
+++ Exercise 3/02.  Sum of absolute numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02.__Sum_of_absolute_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
