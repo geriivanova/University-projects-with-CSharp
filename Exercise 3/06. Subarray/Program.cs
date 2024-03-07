0e90454fa4c54c7be515121b83bdd0f3c495bc57
Gergana Ivanova <geri7423@abv.bg>
2024-03-07 12:44:32 +0200

    finish

--- Exercise 3/06. Subarray/Program.cs
+++ Exercise 3/06. Subarray/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] firstArray = new int[10];
+            int[] secondArray = new int[3];
+
+            Console.WriteLine("Enter first array: ");
+            for(int i = 0; i < firstArray.Length; i++)
+            {
+                firstArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter second array: ");
+            for (int i = 0;i < secondArray.Length;i++)
+            {
+                secondArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            if(secondArray.All(x => firstArray.Contains(x)))
+            {
+                Console.WriteLine("Second array is subarray");
+            }
+            else
+            {
+                Console.WriteLine("Second array is not subarray");
+            }
         }
     }
 }
