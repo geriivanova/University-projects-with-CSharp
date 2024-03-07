a57a72aecc716abd2b84cb86a91d55427e59b7ba
Gergana Ivanova <geri7423@abv.bg>
2024-03-07 11:23:26 +0200

    create task

---
+++ Exercise 3/04. Most common symbol/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04._Most_common_symbol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
