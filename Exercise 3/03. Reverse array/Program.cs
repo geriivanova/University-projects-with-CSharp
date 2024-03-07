caa6dd66588a69dd546e5bd3dc86f5ba75e7c373
Gergana Ivanova <geri7423@abv.bg>
2024-03-07 11:19:50 +0200

    finish project

--- Exercise 3/03. Reverse array/Program.cs
+++ Exercise 3/03. Reverse array/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace _03._Reverse_array
+﻿/*
+4 88 555 999 232 33 3 9 20 34
+ */
+namespace _03._Reverse_array
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = Console.ReadLine()
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+            var reverseArray = numbers.Reverse();
+            Console.Write(string.Join(", ", reverseArray));  
         }
     }
 }
