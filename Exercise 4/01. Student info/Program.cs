ebdd05a60ca2b5811c14e91b5e9af28144870d38
Gergana Ivanova <geri7423@abv.bg>
2024-03-14 08:39:22 +0200

    create

---
+++ Exercise 4/01. Student info/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Student_info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
