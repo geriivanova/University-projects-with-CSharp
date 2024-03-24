cfea09ba87f266512672a2131c51b363c8b87d83
Gergana Ivanova <geri7423@abv.bg>
2024-03-24 20:03:24 +0200

    Update Program.cs

--- Test1Preparation/01. Count of vowels/Program.cs
+++ Test1Preparation/01. Count of vowels/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace _01._Count_of_vowels
+﻿using System.Linq.Expressions;
+
+namespace _01._Count_of_vowels
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string word = Console.ReadLine().ToLower();
+            int countOfVowels = 0;
+
+            for(int i = 0; i < word.Length; i++)
+            {
+                switch(word[i])
+                {
+                    case 'а':
+                        countOfVowels++;
+                        break;
+                    case 'ъ':
+                        countOfVowels++;
+                        break;
+                    case 'о':
+                        countOfVowels++;
+                        break;
+                    case 'у':
+                        countOfVowels++;
+                        break;
+                    case 'е':
+                        countOfVowels++;
+                        break;
+                    case 'и':
+                        countOfVowels++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(countOfVowels);
         }
     }
 }
