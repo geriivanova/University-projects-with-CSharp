b2abfe28a110440cb41de12b1c8a6e63c03df79d
Gergana Ivanova <geri7423@abv.bg>
2024-03-24 20:13:00 +0200

    task 2

---
+++ Test1Preparation/02. Average number/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._Average_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
