d7bd9aee7c01f31f8098a8d5c325b03f1315ae7f
Gergana Ivanova <geri7423@abv.bg>
2024-03-24 19:54:34 +0200

    Create

---
+++ Test1Preparation/01. Count of vowels/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Count_of_vowels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
