d96088690f32524b330d6771938291f84b799966
Gergana Ivanova <geri7423@abv.bg>
2024-03-25 23:48:07 +0200

    finish task

--- Test1Preparation/03. Digit/Program.cs
+++ Test1Preparation/03. Digit/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number = int.Parse(Console.ReadLine());
+            int firstDigit;
+            int lastDigit;
+            for(int i = 10; i <= 99; i++)
+            {
+                lastDigit = i % 10;
+                firstDigit = i / 10;
+                if(firstDigit == number || lastDigit == number)
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
