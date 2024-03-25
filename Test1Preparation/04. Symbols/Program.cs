e6db0a3e5e37dbdb6a8a9206184afcb75e27258b
Gergana Ivanova <geri7423@abv.bg>
2024-03-25 23:57:09 +0200

    finish

--- Test1Preparation/04. Symbols/Program.cs
+++ Test1Preparation/04. Symbols/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            string symbols = string.Empty;
+
+            for(int i = 0; i < n; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+                symbols += symbol;
+            }
+            Console.WriteLine(string.Join(" ", symbols.Reverse()));
         }
     }
 }
