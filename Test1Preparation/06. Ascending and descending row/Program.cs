6ef6077c6a86ee6623fa0ff31a1512cead30039f
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 18:07:32 +0200

    create

---
+++ Test1Preparation/06. Ascending and descending row/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _06._Ascending_and_descending_row
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
