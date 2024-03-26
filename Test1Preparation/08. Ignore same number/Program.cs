ac3124bb40cfaae3512f72148f4da4d2e33bfb2d
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 19:34:35 +0200

    finish

--- Test1Preparation/08. Ignore same number/Program.cs
+++ Test1Preparation/08. Ignore same number/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int firstNumber = int.Parse(Console.ReadLine());
+            int countOfNumbers = 1;
+            int previousNumber = firstNumber;
+            int currentNumber;
+            int minNumber = firstNumber;
+            decimal sumOfNumbers = 0;
+            sumOfNumbers += firstNumber;
+
+
+            for (int i = 2; i <= 10; i++)
+            {
+                currentNumber = int.Parse(Console.ReadLine());
+                
+                if(currentNumber != previousNumber)
+                {
+                    countOfNumbers++;
+                    sumOfNumbers += currentNumber;
+                }
+                if(currentNumber < minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+                previousNumber = currentNumber;
+            }
+            decimal average = (sumOfNumbers / countOfNumbers);
+            Console.WriteLine($"Average is: {average:F2}, minumum is {minNumber}");
         }
     }
 }
