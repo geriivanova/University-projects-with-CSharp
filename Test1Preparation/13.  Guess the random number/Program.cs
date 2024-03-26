85a767936f870774c02bb988ed9172aafc1c2cf0
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 21:51:37 +0200

    new task

---
+++ Test1Preparation/13.  Guess the random number/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _13.__Guess_the_random_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
