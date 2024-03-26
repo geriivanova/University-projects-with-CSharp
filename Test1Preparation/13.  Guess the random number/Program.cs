58e93f37d49ee1e40e5ab5fdcf2e47f89b18d464
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 22:42:26 +0200

    finish task

--- Test1Preparation/13.  Guess the random number/Program.cs
+++ Test1Preparation/13.  Guess the random number/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            int randomNumber = random.Next(1,50);
+
+            for (int i = 0; i < 5; i++)
+            {
+                int numbering = i + 1;
+                Console.Write($"{numbering} try: ");
+                int number = int.Parse(Console.ReadLine());
+                while (number < 1 || number > 50)
+                {
+                    Console.WriteLine("Enter new number between 1 and 50!");
+                    number = int.Parse(Console.ReadLine());
+                }
+                
+                if(number == randomNumber)
+                {
+                    Console.WriteLine($"Congratulations, you guess the random number! The random number is {randomNumber}");
+                    return;
+                }
+                else
+                {
+                    if(numbering < 5)
+                    {
+                        Console.WriteLine("Sorry, you did not guess the number! Try again.");
+                    }
+                }
+            }
+
+            Console.WriteLine($"Game over! The random number is: {randomNumber}");
         }
     }
 }
