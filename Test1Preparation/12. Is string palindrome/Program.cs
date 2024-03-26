81cfac10407e03c58358cd2591ff535ef4f0a1ee
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 21:49:24 +0200

    finish

--- Test1Preparation/12. Is string palindrome/Program.cs
+++ Test1Preparation/12. Is string palindrome/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string word = Console.ReadLine();
+            string reverseString = string.Empty;
+
+            for(int i = word.Length - 1; i >= 0; i--)
+            {
+                reverseString += word[i];
+            }
+
+            if(reverseString == word)
+            {
+                Console.WriteLine($"{word} is palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{word} is not palindrome");
+            }
         }
     }
 }
