a99c2ab94a827b0cb9cc613cd77f0e189dfbd6e5
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 20:39:02 +0200

    Update Program.cs

--- Test1Preparation/11. Count of equal numbers/Program.cs
+++ Test1Preparation/11. Count of equal numbers/Program.cs
@@ -14,10 +14,6 @@
                 {
                     countNumber++;
                 }
-                else
-                {
-                    break;
-                }
             }
             while (newNumber == number);
 
