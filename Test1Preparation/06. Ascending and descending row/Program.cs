3d8e42039b9921d38a6c2359a7ea320d7b1862fa
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 18:33:31 +0200

    finish

--- Test1Preparation/06. Ascending and descending row/Program.cs
+++ Test1Preparation/06. Ascending and descending row/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int previousNumber = int.MinValue;
+            int currentNumber = 0;
+            bool isAscending = true;
+            bool isDescending = true;
+            bool firstNumberEntered = false;
+
+            while (true)
+            {
+                currentNumber = int.Parse(Console.ReadLine());
+
+                if (!firstNumberEntered)
+                {
+                    previousNumber = currentNumber;
+                    firstNumberEntered = true;
+                    continue;
+                }
+
+                if (currentNumber <= 0)
+                {
+                    break;
+                }
+
+                if (currentNumber > previousNumber)
+                {
+                    isDescending = false;
+                }
+                else if (currentNumber < previousNumber)
+                {
+                    isAscending = false;
+                }
+
+                previousNumber = currentNumber;
+            }
+
+            if (isAscending && !isDescending)
+            {
+                Console.WriteLine("The entered numbers form an ascending row.");
+            }
+            else if (isDescending && !isAscending)
+            {
+                Console.WriteLine("The entered numbers form a descending row.");
+            }
+            else
+            {
+                Console.WriteLine("The numbers entered form neither ascending nor descending sequence.");
+            }
         }
     }
 }
