f24c3db7c4176541167da3715fe14f95ce820a2c
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 20:12:22 +0200

    finish task

--- Test1Preparation/10. Ignore negative numbers/Program.cs
+++ Test1Preparation/10. Ignore negative numbers/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int count = 5;
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+
+            while (count > 0)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > 0)
+                {
+                    count--;
+                    if (number < minNumber)
+                    {
+                        minNumber = number;
+                    }
+                    if (number > maxNumber)
+                    {
+                        maxNumber = number;
+                    }
+                    continue;
+                }
+            }
+            Console.WriteLine($"Max number is: {maxNumber}, Min number is: {minNumber}");
         }
     }
 }
