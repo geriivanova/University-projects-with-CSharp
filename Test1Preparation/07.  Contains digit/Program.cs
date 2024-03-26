5733e2cbc098279c71bb3ee99b09439b5549816f
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 18:41:11 +0200

    new task

---
+++ Test1Preparation/07.  Contains digit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07.__Contains_digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
