7ea747e4897566912f6234e223c79919310c5f85
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 20:17:09 +0200

    new task

---
+++ Test1Preparation/11. Count of equal numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _11._Count_of_equal_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
