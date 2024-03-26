4a571058cd3c9a5959b667580a781d031aeb12e1
Gergana Ivanova <geri7423@abv.bg>
2024-03-26 21:35:18 +0200

    create

---
+++ Test1Preparation/12. Is string palindrome/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12._Is_string_palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
