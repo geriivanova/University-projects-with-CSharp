16ede4553358b79b09981f3aca3c7f2a4ed86778
Gergana Ivanova <geri7423@abv.bg>
2024-03-28 22:34:34 +0200

    Update Program.cs

--- Test 1/01. Task 1/Program.cs
+++ Test 1/01. Task 1/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter array size: ");
+            int arraySize = int.Parse(Console.ReadLine());
+            int[] array = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                int element = int.Parse(Console.ReadLine());
+                while (element < -9 || element > 9)
+                {
+                    Console.WriteLine("Enter correct nmber between -9 and 9");
+                    element = int.Parse(Console.ReadLine());
+                }
+                array[i] = element;
+            }
+
+            Console.WriteLine("Array is: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("New array is: ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string concatArray = array[i].ToString() + "1";
+                array[i] = int.Parse(concatArray);
+                Console.Write(array[i] + " ");
+            }
         }
     }
 }
