2b4da9707d503e51df768045a7e468b910d909fa
Gergana Ivanova <geri7423@abv.bg>
2024-03-28 22:33:20 +0200

    create

---
+++ Test 1/01. Task 1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01._Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
