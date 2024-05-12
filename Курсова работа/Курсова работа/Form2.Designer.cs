fed70c1bc1d0f857e6af5ac1a4a06e196cd80821
Gergana Ivanova <geri7423@abv.bg>
2024-05-12 14:52:58 +0300

    Update Form2.Designer.cs

--- Курсова работа/Курсова работа/Form2.Designer.cs
+++ Курсова работа/Курсова работа/Form2.Designer.cs
@@ -36,35 +36,39 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 80);
+            this.label1.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(102, 71);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(365, 16);
+            this.label1.Size = new System.Drawing.Size(600, 27);
             this.label1.TabIndex = 0;
             this.label1.Text = "Намери най-голямо и най-малко количество продажби";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(474, 70);
+            this.button1.BackColor = System.Drawing.Color.IndianRed;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(321, 128);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 37);
+            this.button1.Size = new System.Drawing.Size(138, 48);
             this.button1.TabIndex = 1;
             this.button1.Text = "Търси";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(54, 183);
+            this.listBox1.Location = new System.Drawing.Point(107, 216);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(453, 148);
+            this.listBox1.Size = new System.Drawing.Size(556, 148);
             this.listBox1.TabIndex = 2;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
