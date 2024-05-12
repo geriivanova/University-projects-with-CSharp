198dfb111fbe99f65526b81a6a81b04c0ecb2d80
Gergana Ivanova <geri7423@abv.bg>
2024-05-12 13:54:24 +0300

    update

--- Курсова работа/Курсова работа/Form2.Designer.cs
+++ Курсова работа/Курсова работа/Form2.Designer.cs
@@ -28,19 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Намери най-голямо и най-малко количество продажби";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(474, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Търси";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(54, 183);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(453, 148);
+            this.listBox1.TabIndex = 2;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Form2";
             this.Text = "Най-голямо и най-малко количество продажби";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
--- Курсова работа/Курсова работа/Form2.cs
+++ Курсова работа/Курсова работа/Form2.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,8 +19,22 @@ namespace Курсова_работа
             InitializeComponent();
             this.gl = gl;
         }
-        
 
-        
+        private void FindMinMaxQuantityProducts()
+        {
+            if (gl.sales.Any())
+            {
+                var maxQuantitySale = gl.sales.OrderByDescending(s => s.quantity).FirstOrDefault();
+                var minQuantitySale = gl.sales.OrderBy(s => s.quantity).FirstOrDefault();
+                listBox1.Items.Clear();
+                listBox1.Items.Add($"Най-голямо количество продажби: {maxQuantitySale.stock} {maxQuantitySale.quantity}");
+                listBox1.Items.Add($"Най-малко количество продажби: {minQuantitySale.stock} {minQuantitySale.quantity}");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FindMinMaxQuantityProducts();
+        }
     }
 }
